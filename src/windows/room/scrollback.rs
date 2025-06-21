28c58725f8159b24cef126798cde40a31a01dcd1
vaw <git@nlih.de>
2025-06-21 16:02:07 +0200

    Add `message_time_display` setting

--- src/windows/room/scrollback.rs
+++ src/windows/room/scrollback.rs
@@ -1462,7 +1462,7 @@ impl StatefulWidget for Scrollback<'_> {
             y += 1;
         }
 
-        let msg_width = Message::message_column_width(&state.viewctx, settings);
+        let msg_width = Message::message_column_width(&state.viewctx, &settings.tunables);
 
         // Render image previews after all text lines have been drawn, as the render might draw below the current
         // line.
