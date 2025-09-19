df6716c1cef0838b62ab796ec6f7ea905686e5c1
vaw <git@nlih.de>
2025-09-19 23:38:38 +0200

    Save image previews for display in `Vec`

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1067,7 +1067,7 @@ impl Message {
         info: &'a RoomInfo,
         settings: &'a ApplicationSettings,
         previews: &'a PreviewManager,
-    ) -> (Text<'a>, [Option<ProtocolPreview<'a>>; 2]) {
+    ) -> (Text<'a>, Vec<ProtocolPreview<'a>>) {
         let width = vwctx.get_width();
 
         let style = self.get_render_style(selected, settings);
@@ -1075,12 +1075,17 @@ impl Message {
         let mut text = Text::default();
         let width = fmt.width();
 
+        let mut protos = Vec::new();
+
         // Show the message that this one replied to, if any.
         let reply = self.reply_to().or_else(|| self.thread_root()).map(|e| info.get_event(&e));
-        let proto_reply = reply.as_ref().and_then(|r| {
+        if let Some(r) = reply {
             if let Some(r) = r {
                 // Format the reply header, push it into the `Text` buffer, and get any image.
-                fmt.push_in_reply(r, style, &mut text, info, settings, previews)
+                let proto_reply = fmt.push_in_reply(r, style, &mut text, info, settings, previews);
+                if let Some(proto) = proto_reply {
+                    protos.push(proto)
+                }
             } else {
                 fmt.push_spans(
                     Line::from(vec![
@@ -1092,22 +1097,21 @@ impl Message {
                     style,
                     &mut text,
                 );
-                None
             }
-        });
+        }
 
         // Now show the message contents, and the inlined reply if we couldn't find it above.
         let (msg, proto) = self.show_msg(width, style, settings, previews);
 
         // Given our text so far, determine the image offset.
-        let proto_main = proto.map(|p| {
+        if let Some(p) = proto {
             let y_off = text.lines.len() as u16;
             let x_off = fmt.cols.user_gutter_width(settings);
 
             // Account for extra lines printed before the message;
             let y_off = y_off + fmt.message_start_line();
-            (p, x_off, y_off)
-        });
+            protos.push((p, x_off, y_off));
+        }
 
         fmt.push_text(msg, style, &mut text);
 
@@ -1126,7 +1130,7 @@ impl Message {
             fmt.push_thread_reply_count(thread.len(), &mut text);
         }
 
-        (text, [proto_main, proto_reply])
+        (text, protos)
     }
 
     pub fn show<'a>(
