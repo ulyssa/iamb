2d4084726af1ae95c078300fe82f1d71372553e3
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2025-10-15 04:58:02 -0400

    flake: use downstream package, fix devshell, update lockfile

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
-  description = "iamb";
-  nixConfig.bash-prompt = "\[nix-develop\]$ ";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -14,7 +14,6 @@
       nixpkgs,
       flake-utils,
       rust-overlay,
-      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -22,41 +21,43 @@
         # We only need the nightly overlay in the devShell because .rs files are formatted with nightly.
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        rustNightly = pkgs.rust-bin.nightly."2025-08-31".default;
+        rustNightly = pkgs.rust-bin.nightly."2025-10-15".default;
       in
       with pkgs;
-      {
-        packages.default = rustPlatform.buildRustPackage {
-          pname = "iamb";
-          version = self.shortRev or self.dirtyShortRev;
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+      rec {
+        packages.default = iamb.override (o: {
+          rustPlatform = o.rustPlatform // {
+            buildRustPackage =
+              args:
+              rustPlatform.buildRustPackage (
+                args
+                // rec {
+                  version = self.shortRev or self.dirtyShortRev;
+                  src = builtins.path {
+                    path = ./.;
+                    name = "iamb-${version}-source";
+                  };
+                  cargoLock.lockFile = ./Cargo.lock;
+                  env.VERGEN_GIT_SHA = version;
+                }
+              );
           };
-          nativeBuildInputs = [ pkg-config ];
-          buildInputs =
-            [ openssl ]
-            ++ lib.optionals stdenv.isDarwin (
-              with darwin.apple_sdk.frameworks;
-              [
-                AppKit
-                Security
-                Cocoa
-              ]
-            );
-        };
+        });
 
         devShell = mkShell {
-          buildInputs = [
-            (rustNightly.override {
-              extensions = [
-                "rust-src"
-                "rust-analyzer-preview"
-                "rustfmt"
-                "clippy"
-              ];
-            })
-            pkg-config
+          inputsFrom = [ packages.default ];
+
+          packages = [
+            (lib.hiPrio (
+              rustNightly.override {
+                extensions = [
+                  "rust-src"
+                  "rust-analyzer-preview"
+                  "rustfmt"
+                  "clippy"
+                ];
+              }
+            ))
             cargo-tarpaulin
             cargo-watch
             sqlite
