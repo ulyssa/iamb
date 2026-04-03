ef493ddfd84129f87dfa2125c7edcc0589aeadb2
vaw <git@nlih.de>
2026-04-03 19:01:02 +0200

    Fix joining rooms by alias for new room versions
    The `room_id` for new room versions no longer contains the homeserver.
    Therefore we need to add the alias server to the `via` or just join the
    alias instead of the resolved id.

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ use matrix_sdk::crypto::encrypt_room_key_export;
 use matrix_sdk::ruma::api::client::error::ErrorKind;
 use matrix_sdk::ruma::matrix_uri::MatrixId;
 use matrix_sdk::ruma::{MatrixToUri, MatrixUri, OwnedUserId};
-use matrix_sdk::OwnedServerName;
+use matrix_sdk::{OwnedServerName, RoomState};
 use modalkit::keybindings::InputBindings;
 use rand::{distributions::Alphanumeric, Rng};
 use temp_dir::TempDir;
@@ -198,7 +198,7 @@ fn resolve_mxid(
     via: &[OwnedServerName],
     join_or_create: bool,
 ) -> IambResult<Result<IambId, String>> {
-    let mut room_name = String::new();
+    let room_name;
     let room_id = match id {
         MatrixId::Room(id) => {
             room_name = id.to_string();
@@ -209,13 +209,15 @@ fn resolve_mxid(
             store.application.worker.resolve_alias(alias_id)?
         },
         MatrixId::User(user_id) => {
-            match store.application.worker.client.get_dm_room(&user_id) {
+            let id = match store.application.worker.client.get_dm_room(&user_id) {
                 Some(room) => room.room_id().to_owned(),
                 None if join_or_create => {
                     store.application.worker.join_room(user_id.to_string(), via.to_owned())?
                 },
                 None => return Ok(Err(format!("No dm with {user_id} found. Create new DM?"))),
-            }
+            };
+            room_name = id.to_string();
+            id
         },
         MatrixId::Event(owned_room_or_alias_id, _event_id) => {
             // ignore event id for now
@@ -237,12 +239,11 @@ fn resolve_mxid(
         .application
         .worker
         .client
-        .joined_rooms()
-        .iter()
-        .any(|room| room.room_id() == room_id)
+        .get_room(&room_id)
+        .is_none_or(|room| room.state() != RoomState::Joined)
     {
         if join_or_create {
-            store.application.worker.join_room(room_id.to_string(), via.to_owned())?;
+            store.application.worker.join_room(room_name, via.to_owned())?;
         } else {
             return Ok(Err(format!("Join room {room_name:?}?")));
         }
